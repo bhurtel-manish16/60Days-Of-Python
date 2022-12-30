8b5c5a50e66bb3678825f56b88519f2d90caa7fe
Manish Bhurtel <bhurtel1manish@gmail.com>
2022-12-30 18:34:56 +0545

    dictinoary

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+d1 = {"a":"apple","b":"banana",
+"c":"carrot",
+"e":{1:"mouse",2:"keyboard"}}
+
+# print(d1["e"][2])
+# t = (1,2,3)
+# print(t[0])
+# print(d1["a"])
+# print(d1.values())
+# print(d1.keys()) #unqiue unchangeble
+# d1.update({"d":"dance"})
+# print(d1)
+# # del d1["b"] #b
+# # d1.popitem()#last key 0> d
+# # print(d1)
+# for i in d1:
+#     # print(i)
+#     if "b" == i:
+#         print(d1["b"])
+# # d2 = d1
+# d2 = d1.copy()
+# del d1["a"]
+# print(d2)
+
+d2 = {
+    1:"keyboard",
+    2:"mouse",
+    3:{1:"antivirus",2:"games"},
+    4:True
+}
+print(d2[3][1])
+print(d2)
