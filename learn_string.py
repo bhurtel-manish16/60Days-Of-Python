bf4f6f8053e922f50a68300e9d73f87a7d788f24
Manish Bhurtel <bhurtel1manish@gmail.com>
2023-01-02 20:53:55 +0545

    Python strng

---
+++ learn_string.py
@@ -0,0 +1,69 @@
+# #var = "Python \"is fun!"
+# var = 'I\'ll do it'
+# var1 = 'Python is fun!'
+# var2 = "Hi I am manish.\n And this is fun!"
+
+# msg = """
+# Hi 
+
+# hello
+
+# hey
+# """
+# print(msg)
+# #var3 = "I will be a.. 24\\4 " 
+# #print(var3)
+# print(var)
+# print(var2)
+# print(len(var)) #function
+# #output -> python ""is fun I'll do it
+#Hellooooo -> HELLOOOO -> HELLO
+#input -> UPPER -> 5 Hi I am manish -> HI I MA MANISH -> HI I 
+# inp = input("Enter a message: ").upper()
+# print(inp[0:5])
+
+# st = input ("enter your message : \n")
+# st.upper()
+# print(st[0:5])
+
+# var = "     python is fun     "
+# #name = "\t\t\tmanish" #tab
+# # print(name)
+# fname = "manish"
+# lname = " bhurtel"
+# fullname = fname + lname
+# print(fullname.title())
+# print(fname+lname)
+# print(var)
+# print(var.lstrip()) #lstrip -> left rstrip() ->right
+
+
+
+#split
+# print(var.split(" ")[0])
+# #print(var)
+# #slicing
+# print(var[0:6])
+# print(var.replace(" ",","))
+# print(name.capitalize())
+# print(name.upper())
+# print(last.lower())
+
+
+# fname = input("Enter your name: ")
+# lname = input("Enter your last name: ")
+# fullname = fname+" "+lname
+# print("Hello "+fullname.title()+" Welcome back")
+#manish bhurtel
+#Hello Manish Bhurtel welcome back!
+fname=input("Enter first name :").capitalize()
+lname=input("Enter last name: ").capitalize()
+status = True
+a = 2
+b = 4
+#This shows value of a and b and displays the sum
+print(f"Sum of {a} and {b} is {a+b}")
+
+fullname = fname+" "+lname
+#print("Welcome " +fname+" "+lname+ " to Facebook")
+print(f"Hello {status} welcome back!")
