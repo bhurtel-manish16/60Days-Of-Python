6412f2b04bdd536abbb4712c4a7fa319d96adb56
Manish Bhurtel <bhurtel1manish@gmail.com>
2023-01-07 12:27:59 +0545

    Lambda function

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+# def xya(x,y):
+#     return x+y
+
+
+#f_name = lambda para:exp
+add = lambda x,y:x+y
+print(add(1,2))
+
+l1 = [1,2,3,4,5,6] #even ->2,4,6
+l2 = list(filter(lambda x:x%2==0,l1))
+print(l2)
